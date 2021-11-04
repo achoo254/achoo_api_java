49dbcb0553ff29f3ba18e80b79a7f808a16c8f7d
Trinh Quoc Hoan Dat <quocdat254@gmail.com>
2021-11-04 11:28:32 +0700

    Initial commit

---
+++ src/main/java/com/achooapi/achoo_api_java/AchooApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.achooapi.achoo_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchooApiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchooApiJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/achooapi/achoo_api_java/AchooApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achooapi.achoo_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchooApiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
